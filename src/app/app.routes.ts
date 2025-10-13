b8bb40860c8e6e3fa26a0f8512bcb24f93a1a49a
fatmayildirim <123fatma.yldrm@gmail.com>
2025-10-13 10:29:15 +0300

    yarışma sayfası düzenlendi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,19 @@ import { Homepage } from './homepage/home';
 import { Layout } from './layout/layout';
 import { Basvuru } from './basvuru/basvuru';
 import { Proje } from './proje/proje';
+import { Yarismalar } from './yarismalar/yarismalar'; // sonradan ekledim 
+
+
 export const routes: Routes = [
   {
     path: '',
     component: Layout,
     children: [
-      { path: '', component: Homepage }, // homepage rotası
-      {path: 'Basvuru',component:Basvuru},
-      {path :'Proje',component:Proje},
-      { path: '**', redirectTo: '', pathMatch: 'full' } 
-     
+      { path: '', component: Homepage },
+      { path: 'Basvuru', component: Basvuru },
+      { path: 'Proje', component: Proje },
+      { path: 'yarismalar', component: Yarismalar },// sonradan ekledim 
+
     ]
   }
-];
+]; 
