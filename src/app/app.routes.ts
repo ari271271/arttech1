b0aca933265bb1d6f9952420ee714c46d9088360
hiilalb <152510629+hiilalb@users.noreply.github.com>
2025-10-19 12:55:47 +0300

    hakkımızda dosyasını hakkimizda olarak düzelttim ve routing sistemine ekledim

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,23 @@ import { Layout } from './layout/layout';
 import { Basvuru } from './basvuru/basvuru';
 import { Proje } from './proje/proje';
 import { Yarismalar } from './yarismalar/yarismalar'; // sonradan ekledim 
-
+import { Hakkimizda } from './hakkimizda/hakkimizda';
 
 export const routes: Routes = [
   {
     path: '',
     component: Layout,
     children: [
+
       { path: '', component: Homepage },
       { path: 'Basvuru', component: Basvuru },
       { path: 'Proje', component: Proje },
       { path: 'yarismalar', component: Yarismalar },// sonradan ekledim 
+      {path: 'hakkimizda', component: Hakkimizda},
+ { path: '**', redirectTo: '', pathMatch: 'full' } 
+
+
+     
 
     ]
   }
---
+++ src/app/hakkimizda/hakkimizda.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-hakkimizda',
+  standalone: true,
+  imports: [RouterModule],
+ templateUrl: './hakkimizda.html',
+    styleUrls: ['./hakkimizda.css'] 
+})
+export class Hakkimizda {}
