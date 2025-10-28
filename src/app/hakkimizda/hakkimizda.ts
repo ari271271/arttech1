19939b414f89c59eaf7f0000fb3b2693d785dcf0
ahmed adnan <ariadnanmanaf@gmail.com>
2025-10-28 13:24:25 +0300

    hakkımızdaki yeri ekleme

--- src/app/hakkimizda/hakkimizda.ts
+++ src/app/hakkimizda/hakkimizda.ts
@@ -9,30 +9,27 @@ import { CommonModule } from '@angular/common';
  templateUrl: './hakkimizda.html',
     styleUrls: ['./hakkimizda.css'] 
 })
-export class Hakkimizda {
-  team = [
-    { name: "BAYRAM BALA", title: "Danışman Hocamız", photo: "/varsayilan.jpg" },
-    { name: "Yunus Ethem TAŞ", title: "CEO (Chief Executive Officer)", photo: "/Yunus.jpeg" },
-    { name: "Nursezen SAĞDIÇ", title: "COO (Chief Operating Officer)", photo: "/Nursezen.jpeg" },
-    { name: "Sena BULUT", title: "Finans Direktörü", photo: "/varsayilan.jpg" },
-    { name: "Eda GÖZLÜKLÜ", title: "Endüstriyel Süreç ve Raporlama Koordinatörü", photo: "/Eda.jpeg" },
-    { name: "Ezgi GEZEZ", title: "Sosyal Medya Sorumlusu", photo: "/Ezgi.jpeg" },
-    { name: "Sidra KAYA", title: "Sosyal Medya Sorumlusu", photo: "/varsayilan.jpg" },
-    { name: "Ahmet ADNAN", title: "Yazılım Geliştirici & Teknik Destek", photo: "/Ahmet.jpeg" },
-    { name: "Zemzem BOYLUĞ", title: "Yazılım Kaptanı", photo: "/Zemzem.jpeg" },
-    { name: "Fehmi SEZER", title: "Elektronik Kaptanı", photo: "/varsayilan.jpg" },
-    { name: "Bumin Kağan KURT", title: "Mekanik Kaptanı", photo: "/varsayilan.jpg" },
-    { name: "Orhan YAŞAR", title: "Elektronik Asistanı", photo: "/Orhan.jpeg" },
-    { name: "Mustafa YILDIRIMER", title: "Elektrik-Elektronik Destek Uzmanı", photo: "/Mustafa.jpeg" },
-    { name: "Büşra KURT", title: "Hatay Temsilcisi", photo: "/varsayilan.jpg" },
-    { name: "Ezel İLHAN", title: "Laboratuvar Destek Üyesi", photo: "/Ezel.jpeg" },
-    { name: "Bedriye TATLI", title: "Elektrik-Elektronik Proje Asistanı", photo: "/Bedriye.jpeg" },
-    { name: "Yağmur KAYA", title: "İnsan Kaynakları", photo: "/varsayilan.jpg" }
-  ];
-
-  onImageError(event: Event) {
-    const imgElement = event.target as HTMLImageElement;
-    imgElement.src = '/varsayilan.jpg';
-  }
-}
+export class Hakkimizda {   team = [
+    { name: "BAYRAM BALA", title: "Danışman Hocamız", photo: "/Bayram.jpeg" },
+  { name: "Yunus Ethem Taş", title: "CEO (Chief Executive Officer)", photo: "/Yunus.jpeg" },
+  { name: "Nursezen Sağdıç", title: "COO (Chief Operating Officer)", photo: "/Nursezen.jpeg" },
+  { name: "Sena", title: "Finans Direktörü", photo: "/Sena.jpeg" },
+  { name: "Eda", title: "Endüstriyel Süreç ve Raporlama Koordinatörü", photo: "/Eda.jpeg" },
+  { name: "Ezgi", title: "Sosyal Medya Sorumlusu", photo: "/Ezgi.jpeg" },
+  { name: "Sidra", title: "Sosyal Medya Sorumlusu", photo: "/Sidra.jpg" },
+  { name: "Ahmet", title: "Yazılım Geliştirici & Teknik Destek", photo: "/Ahmet.jpeg" },
+  { name: "Zemzem", title: "Yazılım Kaptanı", photo: "/Zemzem.jpeg" },
+  { name: "Fehmi", title: "Elektronik Kaptanı", photo: "/Fehmi.jpeg" },
+  { name: "Bumin Kağan", title: "Mekanik Kaptanı", photo: "/Bumin.jpeg" },
+  { name: "Orhan", title: "Elektronik Asistanı (Elektronik Mühendisliği Öğrencisi)", photo: "/Orhan.jpeg" },
+  { name: "Mustafa", title: "Elektrik-Elektronik Destek Uzmanı", photo: "/Mustafa.jpeg" },
+  { name: "Büşra", title: "Hatay Temsilcisi", photo: "/Busra.jpeg" },
+  { name: "Ezel", title: "Laboratuvar Destek Üyesi", photo: "/Ezel.jpeg" },
+  { name: "Bedriye", title: "Elektrik-Elektronik Proje Asistanı", photo: "/Bedriye.jpeg" },
+  { name: "Yağmur", title: "Insan kaynakları", photo: "/yagmur.jpeg" }
+];
+onImageError(event: Event) {
+  const imgElement = event.target as HTMLImageElement;
+  imgElement.src = '/varsayilan.jpg';
+}}
 
