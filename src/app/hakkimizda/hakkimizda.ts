bcb61556fcc49d746ecf37eebbf756893a647719
hiilalb <152510629+hiilalb@users.noreply.github.com>
2025-10-28 20:50:54 +0300

    bazı fotoğraflar eklendi

--- src/app/hakkimizda/hakkimizda.ts
+++ src/app/hakkimizda/hakkimizda.ts
@@ -10,16 +10,16 @@ import { CommonModule } from '@angular/common';
     styleUrls: ['./hakkimizda.css'] 
 })
 export class Hakkimizda {   team = [
-    { name: "BAYRAM BALA", title: "Danışman Hocamız", photo: "/Bayram.jpeg" },
-  { name: "Yunus Ethem Taş", title: "CEO (Chief Executive Officer)", photo: "/Yunus.jpeg" },
-  { name: "Nursezen Sağdıç", title: "COO (Chief Operating Officer)", photo: "/Nursezen.jpeg" },
+    { name: "BAYRAM BALA", title: "Danışman Hocamız", photo: "/BayramBala.jpeg" },
+  { name: "Yunus Ethem TAŞ", title: "CEO (Chief Executive Officer)", photo: "/Yunus.jpeg" },
+  { name: "Nursezen SAĞDIÇ", title: "COO (Chief Operating Officer)", photo: "/Nursezen.jpeg" },
   { name: "Sena", title: "Finans Direktörü", photo: "/Sena.jpeg" },
-  { name: "Eda", title: "Endüstriyel Süreç ve Raporlama Koordinatörü", photo: "/Eda.jpeg" },
+  { name: "Eda GÖZLÜKLÜ", title: "Endüstriyel Süreç ve Raporlama Koordinatörü", photo: "/Eda.jpeg" },
   { name: "Ezgi", title: "Sosyal Medya Sorumlusu", photo: "/Ezgi.jpeg" },
   { name: "Sidra", title: "Sosyal Medya Sorumlusu", photo: "/Sidra.jpg" },
-  { name: "Ahmet", title: "Yazılım Geliştirici & Teknik Destek", photo: "/Ahmet.jpeg" },
-  { name: "Zemzem", title: "Yazılım Kaptanı", photo: "/Zemzem.jpeg" },
-  { name: "Fehmi", title: "Elektronik Kaptanı", photo: "/Fehmi.jpeg" },
+  { name: "Ahmet ADNAN", title: "Yazılım Geliştirici & Teknik Destek", photo: "/Ahmet.jpeg" },
+  { name: "Zemzem BOYLUĞ", title: "Yazılım Kaptanı", photo: "/Zemzem.jpeg" },
+  { name: "Fehmi SEZEN", title: "Elektronik Kaptanı", photo: "/Fehmi.jpeg" },
   { name: "Bumin Kağan", title: "Mekanik Kaptanı", photo: "/Bumin.jpeg" },
   { name: "Orhan", title: "Elektronik Asistanı (Elektronik Mühendisliği Öğrencisi)", photo: "/Orhan.jpeg" },
   { name: "Mustafa", title: "Elektrik-Elektronik Destek Uzmanı", photo: "/Mustafa.jpeg" },
