e3d0e97261868001afcef2b0e47fbc14f6851245
ahmed adnan <ariadnanmanaf@gmail.com>
2025-10-30 09:38:51 +0300

    hakkımızdaki soy isimler ekleme

--- src/app/hakkimizda/hakkimizda.ts
+++ src/app/hakkimizda/hakkimizda.ts
@@ -10,7 +10,8 @@ import { CommonModule } from '@angular/common';
     styleUrls: ['./hakkimizda.css'] 
 })
 export class Hakkimizda {   team = [
-    { name: "BAYRAM BALA", title: "Danışman Hocamız", photo: "/BayramBala.jpeg" },
+    { name: "BAYRAM BALA", title: "Topluluk Danışmanımız ", photo: "/BayramBala.jpeg" },
+    { name: "IPEK ATIK", title: "Proje Danışmanımız ", photo: "/BayramBala.jpeg" },
   { name: "Yunus Ethem TAŞ", title: "CEO (Chief Executive Officer)", photo: "/Yunus.jpeg" },
   { name: "Nursezen SAĞDIÇ", title: "COO (Chief Operating Officer)", photo: "/Nursezen.jpeg" },
   { name: "Sena BULUT", title: "Finans Direktörü", photo: "/Sena.jpeg" },
