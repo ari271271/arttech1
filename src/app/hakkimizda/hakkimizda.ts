2db567595db6330f566064fef6330dcb47f69b05
ahmed adnan <ariadnanmanaf@gmail.com>
2025-12-02 23:50:49 +0300

    hakkımızda

--- src/app/hakkimizda/hakkimizda.ts
+++ src/app/hakkimizda/hakkimizda.ts
@@ -31,91 +31,91 @@ export class Hakkimizda {
     },
     { 
         name: "Yunus Ethem TAŞ", 
-        title: "CEO (Chief Executive Officer)", 
+        title: "ArtTech Şirket ve Topluluk Başkanı", 
         photo: "/Yunus.jpeg", 
-        dialogInfo: "ArtTech'in kurucusu ve lideri olarak, sanat ve teknolojiyi birleştirme vizyonuyla hareket etmektedir. Ekibin motivasyonunu en üst seviyede tutarak, şirketin global hedeflere ulaşması için stratejik kararlar alır ve yenilikçi bir çalışma kültürü oluşturur." 
+        dialogInfo: "Gaziantep İslâm Bilim Ve Teknoloji Üniversitesi Elektrik Elektronik Mühendisliği öğrencisiyim. ArtTech Şirket ve Topluluk Başkanı olarak sanat ve teknolojiyi birleştirme vizyonuyla hareket etmektedir. Ekibin motivasyonunu en üst seviyede tutarak, şirket ve topluluğun global hedeflere ulaşması için stratejik kararlar alır ve yenilikçi bir çalışma kültürü oluşturur." 
     },
     { 
         name: "Nursezen SAĞDIÇ", 
         title: "Başkan Yardımcısı", 
         photo: "/Nursezen.jpeg", 
-        dialogInfo: "Gaziantep İslâm Bilim ve Teknoloji Üniversitesi Endüstri Mühendisliği öğrencisiyim. ArtTech’te Başkan Yardımcısı olarak görev almaktayım. Şirketimizin operasyonel süreçlerini yönetiyor, verimliliği artırmak ve sürdürülebilir büyümeyi desteklemek adına stratejik çalışmalar yürütüyorum. Sistematik düşünme, süreç iyileştirme ve yenilikçi operasyon yönetimi yaklaşımlarıyla şirketin işleyişini daha etkili ve optimize bir hâle getirmeyi hedefliyorum. Kendimi geliştirmeyi, öğrenmeyi ve her yaptığım işte en iyisini ortaya koymayı önemsiyorum. ArtTech’in gelişen vizyonuna değer katmak ve şirketin geleceğine yön veren güçlü bir operasyonel yapı oluşturmak için tutkuyla çalışıyorum." 
+        dialogInfo: "Gaziantep İslâm Bilim ve Teknoloji Üniversitesi Endüstri Mühendisliği öğrencisiyim. ArtTech’te Başkan Yardımcısı olarak görev almaktayım. Şirket ve topluluğumuzun operasyonel süreçlerini yönetiyor, verimliliği arttırmak ve sürdürülebilir büyümeyi desteklemek adına stratejik çalışmalar yürütüyorum. ." 
     },
     { 
         name: "Sena BULUT", 
         title: "Finans Direktörü", 
         photo: "/Sena.jpeg", 
-        dialogInfo: "Gaziantep İslâm Bilim Ve Teknoloji Üniversitesi Endüstri Mühendisliği öğrencisi olarak ArtTech'de CFO (Chief Financial Officer) görevini yürütmektedir. Şirketin finansal stratejilerini yönetir, sürdürülebilir büyüme ve verimlilik hedefleri doğrultusunda çalışmalar yürütür. Analitik düşünme, stratejik planlama ve inovatif finansal yaklaşımlarla şirketin geleceğine yön vermeyi hedefleyen Sena, sürekli gelişime ve yaptığı her işte içtenlikle ilerlemeye önem verir." 
+        dialogInfo: "Gaziantep İslâm Bilim Ve Teknoloji Üniversitesi Endüstri Mühendisliği öğrencisiyim. ArtTech'te CFO (Chief Financial Officer) görevini yürütüyorum Şirketin finansal stratejilerini yönetir, sürdürülebilir büyüme ve verimlilik hedefleri doğrultusunda çalışmalar yürütüyorum." 
     },
     { 
         name: "Eda GÖZLÜKLÜ", 
         title: "Endüstriyel Süreç ve Raporlama Koordinatörü", 
         photo: "/Eda.jpeg", 
-        dialogInfo: "Projelerin endüstriyel standartlara uygunluğunu ve süreçlerin şeffaflığını sağlamaktan sorumludur. Detaylı raporlama ve sürekli iyileştirme prensipleriyle ArtTech projelerinin profesyonel kalitesini ve verimliliğini maksimize eder." 
+        dialogInfo: "Gaziantep İslâm Bilim Ve Teknoloji Üniversitesi Endüstri Mühendisliği öğrencisiyim. ArtTech'te projelerin endüstriyel standartlara uygunluğunu ve süreçlerin şeffaflığını sağlamaktan sorumluyumdur. Detaylı raporlama ve sürekli iyileştirme prensipleriyle ArtTech projelerinin profesyonel kalitesini ve verimliliğini maksimize ederim." 
     },
     { 
         name: "Ahmet Eren Ebe", 
         title: "Sponsorluk İlişkileri Yöneticisi", 
         photo: "/AhmetEren.jpeg", 
-        dialogInfo: "Gaziantep İslam Bilim ve Teknoloji Üniversitesi Elektrik-Elektronik Mühendisliği 1. Sınıf Öğrencisiyim. ArtTech Bünyesinde Sponsorluk İlişkileri Yöneticisi Olarak Görev Yapmaktayım. Görevim Bizim Projelerimizi En Doğru Şekilde Temsil Edip Sponsorlarla Sürekli İletişimde Olan, Onlara İhtiyaçlarımızı, İlerlememizi Ve Vizyonumuzu Aktaran Kişiyim. Ekip Arkadaşlarım Sahada Çalışırken, Bende Dışarıda Ekibin Sesi Oluyorum. Bu Rolde, Güven Ve Profesyonelliği Yansıtarak, Fırsatları Takip Etmek Ve Güçlü İş Birlikleri Kurmak Benim Görevimdir."
+        dialogInfo: "Gaziantep İslâm Bilim Ve Teknoloji Üniversitesi Elektrik Elektronik Mühendisliği öğrencisiyim. ArtTech'te sponsorluk İlişkileri Yöneticisi Olarak Görev Yapmaktayım. Görevim Bizim Projelerimizi En Doğru Şekilde Temsil Edip Sponsorlarla Sürekli İletişimde Olan, Onlara İhtiyaçlarımızı, İlerlememizi Ve Vizyonumuzu Aktaran Kişiyim. Ekip Arkadaşlarım Sahada Çalışırken, Bende Dışarıda Ekibin Sesi Oluyorum."
     },
     { 
         name: "Ezgi GEGEZ", 
         title: "Sosyal Medya Koordinatörü", 
         photo: "/Ezgi.jpeg", 
-        dialogInfo: "Gaziantep İslam Bilim ve Teknoloji Üniversitesi, 3. sınıf Endüstri Mühendisliği öğrencisiyim. Kendimi her alanda geliştirmeyi hedefliyor ve bu doğrultuda çeşitli alanlarda çaba sarf ediyorum. Şu anda ArtTech topluluğunda Sosyal Medya Koordinatörü olarak görev alıyorum. İçerik üretimi ve fotoğrafçılıkla ilgileniyor, yaratıcı projelerde aktif rol almayı seviyorum. Bu süreçte, hem kişisel hem de profesyonel anlamda kendimi daha da ileriye taşımayı amaçlıyorum." 
+        dialogInfo: "Gaziantep İslâm Bilim ve Teknoloji Üniversitesi Endüstri Mühendisliği öğrencisiyim. ArtTech'te Sosyal Medya Koordinatörü olarak görev alıyorum. İçerik üretimi ve fotoğrafçılıkla ilgileniyorum. Sosyal medya trendlerini takip ederek, etkili ve özgün içerikler oluşturmayı hedefliyorum." 
     },
     // Sidra AĞAOĞLU ÇIKARILDI
     { 
         name: "Ahmet ADNAN MANAF", 
         title: "Yazılım Geliştirici & Teknik Destek", 
         photo: "/Ahmet.jpeg", 
-        dialogInfo: "Yazılım projelerinde teknik uzmanlığıyla çözümler üretir ve ekibe destek olur. Karşılaşılan teknik zorluklara pratik ve yenilikçi yaklaşımlar getirerek projelerin kesintisiz ilerlemesini sağlar." 
+        dialogInfo: "Gaziantep İslâm Bilim ve Teknoloji Üniversitesi Bilgisayar Mühendisliği öğrencisiyim. ArtTech'te yazılım projelerinde teknik uzmanlık sağlıyor ve ekibe destek oluyorum. Karşılaştığımız teknik zorluklara pratik ve yenilikçi çözümler getirerek projelerin sorunsuz bir şekilde ilerlemesini sağlıyorum." 
     },
     { 
         name: "Zemzem BOYLUĞ", 
         title: "Yazılım Kaptanı", 
         photo: "/Zemzem.jpeg", 
-        dialogInfo: "Yazılım ekibinin lideri olarak, kodlama standartlarını belirler ve teknik mimariye yön verir. Ekip üyelerinin gelişimine odaklanarak, ArtTech'in tüm yazılım çözümlerinin en yüksek kalitede olmasını sağlar." 
+        dialogInfo: "Gaziantep İslâm Bilim ve Teknoloji Üniversitesi Bilgisayar Mühendisliği öğrencisiyim. ArtTech'te yazılım ekibinin lideri olarak, kodlama standartlarını belirliyor ve teknik mimariyi şekillendiriyorum. Ekip üyelerinin gelişimine odaklanarak, ArtTech'in yazılım çözümlerinin en yüksek kaliteye ulaşmasını sağlıyorum." 
     },
     // Fehmi SEZER ÇIKARILDI
     // Bumin Kağan DOĞAN ÇIKARILDI
     { 
         name: "Muhammed Orhan YAŞAR", 
-        title: "Elektronik Asistanı (Elektronik Mühendisliği Öğrencisi)", 
+        title: "Elektronik Asistanı ", 
         photo: "/Orhan.jpeg", 
-        dialogInfo: "Gaziantep İslam Bilim ve Teknoloji Üniversitesi Elektrik-Elektronik Mühendisliği 1. sınıf öğrencisiyim. ArtTech bünyesinde Elektronik Asistanı olarak görev yapmaktayım. Elektronik devre tasarımı, sistem analizi ve otomasyon teknolojileri üzerine çalışıyor; mühendislik alanındaki akademik bilgilerimi sahadaki uygulamalarla birleştirerek projelere katkı sağlamayı hedefliyorum. Yenilikçi teknolojilere ilgi duyan, araştırmacı ve çözüm odaklı bir mühendis adayı olarak, ArtTech’in teknolojiye yön veren vizyonuna katkıda bulunmak benim için büyük bir motivasyon kaynağıdır." 
+        dialogInfo: "Gaziantep İslâm Bilim Ve Teknoloji Üniversitesi Elektrik Elektronik Mühendisliği öğrencisiyim. ArtTech'te Elektronik Asistanı olarak görev yapmaktayım. Elektronik devre tasarımı, sistem analizi ve otomasyon teknolojileri üzerine çalışıyor; mühendislik alanındaki akademik bilgilerimi sahadaki uygulamalarla birleştirerek projelere katkı sağlamayı hedefliyorum." 
     },
     { 
         name: "Mustafa YILDIRIMER", 
         title: "Elektrik-Elektronik Destek Uzmanı", 
         photo: "/Mustafa.jpeg", 
-        dialogInfo: "Özel elektrik ve elektronik konularda derinlemesine destek sağlar. Karmaşık teknik sorunları çözme yeteneği ile projelere teknik güvenilirlik katar." 
+        dialogInfo: " Gaziantep İslâm Bilim Ve Teknoloji Üniversitesi Elektrik Elektronik Mühendisliği öğrencisiyim. ArtTech'te özel elektrik ve elektronik konularda derinlemesine destek sağlar. Karmaşık teknik sorunları çözme yeteneği ile projelere teknik güvenilirlik katarım." 
     },
     { 
         name: "Büşra KURT", 
         title: "Hatay Temsilcisi", 
         photo: "/Busra.jpeg", 
-        dialogInfo: "ArtTech'i Hatay bölgesinde temsil eder ve yerel etkinlikleri organize eder. Bölgesel projelerde ArtTech'in vizyonunu ve misyonunu yayarak topluluğun etki alanını genişletir." 
+        dialogInfo: "İskenderun Teknik Üniversitesi Bilgisayar Mühendisliği öğrencisiyim. ArtTech Hatay Temsilcisi olarak, topluluğumuzun bölgede daha fazla görünür olmasına katkı sağlamaya çalışıyorum. Bölgesel projelerde, ArtTech'in vizyonunu ve misyonunu benimseyerek topluluğumuzun etki alanını genişletiyorum." 
     },
     { 
         name: "Ezel ILHAN", 
         title: "Laboratuvar Destek Üyesi", 
         photo: "/Ezel.jpeg", 
-        dialogInfo: "Gaziantep İslam Bilim ve Teknoloji Üniversitesi Tıbbi Laboratuvar Teknikleri bölümünde öğrenim görmekteyim ve Art Tech'te Laboratuvar Destek Üyesi olarak görev alıyorum. Laboratuvar ortamında teknik bilgi ve pratiklerimle destek sunuyorum. Ayrıca döngüsel ekonomi alanındaki bilgi birikimimi gençlerle yapılan çalışmalarda paylaşarak sürdürülebilir farkındalık oluşmasına katkı sağlıyorum. Enerjim ve motivasyonumla bulunduğum her ekibe değer katmayı hedefliyorum." 
+        dialogInfo: "Gaziantep İslâm Bilim Ve Teknoloji Üniversitesi Tıbbi Laboratuvar Teknikleri öğrencisiyim. ArtTech'te Laboratuvar Destek Üyesi olarak görev alıyorum. Laboratuvar ortamında teknik bilgi ve pratiklerimle destek sunuyorum. ." 
     },
     // Bedriye TATLI ÇIKARILDI
     { 
         name: "Yağmur ÇINAR", 
         title: "Insan kaynakları", 
         photo: "/Yagmur.jpeg", 
-        dialogInfo: "İnsan kaynakları süreçlerini yönetir, yeni üye alımı ve ekip motivasyonundan sorumludur. ArtTech'in güçlü ve uyumlu bir ekibe sahip olması için stratejik çalışmalar yürütür." 
+        dialogInfo: "Gaziantep İslâm Bilim ve Teknoloji Üniversitesi Elektrik Elektronik Mühendisliği öğrencisiyim. ArtTech'te insan kaynakları süreçlerini yönetiyor, yeni üye alımı ve ekip motivasyonuyla ilgileniyorum. ArtTech'in güçlü ve uyumlu bir ekibe sahip olması için stratejik çalışmalar yürütüyorum." 
     },
     { 
         name: "Merve AKYÜZ", 
         title: "Proje Müdürü", 
         photo: "/Merve.jpeg", 
-        dialogInfo: "Topluluğun ana projelerinin zamanında ve bütçe dahilinde tamamlanmasını sağlar. Mükemmel organizasyon ve iletişim becerileriyle proje ekiplerini başarıya ulaştırır." 
+        dialogInfo: "Gaziantep İslâm Bilim ve Teknoloji Üniversitesi Endüstri Mühendisliği öğrencisiyim. ArtTech'te, topluluğun ana projelerinin zamanında ve bütçe dahilinde tamamlanmasını sağlıyorum. Mükemmel organizasyon ve iletişim becerilerimle proje ekiplerini başarıya ulaştırıyorum." 
     }
   ];
 
